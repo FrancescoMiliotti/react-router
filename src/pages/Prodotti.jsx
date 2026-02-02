fd9048d2f995e339f4d67b56b9bcb49c8cf0ee8d
francesco.miliotti <francesco.miliotti@email.com>
2026-02-02 22:19:31 +0100

    card prodotti

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,31 +1,72 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios";
 import NavBar from "../components/NavBar"
 
 
 function Prodotti() {
 
+    const [products, setProduct] = useState([]);
+
     function mounted() {
         console.log("Applicazione montata correttamente");
+
 
         const apiurl = "https://fakestoreapi.com/products";
 
         axios.get(apiurl).then(res => {
             console.log("Ricevuti dati da API:", res.data);
+            setProduct(res.data);
+
+        }).catch(error => {
+            console.log("Qualcosa è andato storto!:", error.message);
+
         })
 
 
-        
     }
-
 
     useEffect(mounted, []);
 
-    return <div>
-        <NavBar />
-        <h1>Qui trovate i nostri Prodotti</h1>
+    return (
+        <div>
+            <NavBar />
 
-    </div>
+            <h1>Qui trovate i nostri Prodotti</h1>
+            <div className="flex-container flex-wrap gap-1">
+                {products.map((product,i) =>
+                    <div className="card" key={i}>
+                        {product.title}
+                        <img src={product.image} className="image" />
+                        <p>{product.description}</p>
+                    </div>)}
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Prodotti
+
+
+
+
+
+/*<div>
+            <h1>Hello React World</h1>
+
+            <h2>Attrici / Attori</h2>
+            <div className="flex-container flex-wrap gap-1">
+                {fullList.map((attrice, i) =>
+                    <div className="card" key={i}>
+                        <img src={attrice.image} className="image" />
+                        <h3>{i + 1}. {attrice.name}</h3>
+                        <p>{attrice.birth_year} - {attrice.nationality}</p>
+                        <p>{attrice.biography}</p>
+                        <p>Premi: {getPremi(attrice.awards)}</p>
+                        <div>Film famosi: {getMovies(attrice)}</div>
+                    </div>
+                )}
+            </div>
+
+        </div>*/
