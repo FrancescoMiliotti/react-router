8cf61e00d625c496e616a6099bb0bad5568d8d0f
francesco.miliotti <francesco.miliotti@email.com>
2026-02-02 18:21:16 +0100

    personalizzo NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import { Link, NavLink } from "react-router-dom"
 
 
 function NavBar() {
-    return <div>
-        <NavLink className="btn" to="/Prodotti">Vai ai Prodotti</NavLink>
-        <NavLink className="btn" to="/ChiSiamo">Vai a Chi Siamo</NavLink>
-        <NavLink className= "btn" to="/">Vai alla HomePage</NavLink>
-</div>
+    return <nav className="navbar">
+        <NavLink className="btn" to="/Prodotti">Prodotti</NavLink>
+        <NavLink className="btn" to="/ChiSiamo">Chi Siamo</NavLink>
+        <NavLink className= "btn" to="/">HomePage</NavLink>
+</nav>
 }
 
     export default NavBar
