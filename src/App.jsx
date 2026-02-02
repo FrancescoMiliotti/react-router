fd2a9626bda6fce5cb62eea7f2d39675ac43f3e4
francesco.miliotti <francesco.miliotti@email.com>
2026-02-02 17:37:35 +0100

    creo le pagine e testo che funzioni

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import axios from "axios";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
+import ChiSiamo from "./pages/ChiSiamo";
+import Prodotti from "./pages/Prodotti";
 
 
 
@@ -11,6 +13,8 @@ function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/" Component={HomePage} />
+      <Route path="/ChiSiamo" Component={ChiSiamo} />
+      <Route path="/Prodotti" Component={Prodotti} />
     </Routes>
 
 
---
+++ src/pages/Prodotti.jsx
@@ -0,0 +1,5 @@
+function Prodotti() {
+    return <div>Prodotti</div>
+}
+
+export default Prodotti
