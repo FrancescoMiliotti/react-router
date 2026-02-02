0fe38f9a34b87497d987b02c488d1a4702b99b8b
francesco.miliotti <francesco.miliotti@email.com>
2026-02-02 17:30:57 +0100

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
+import axios from "axios";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+
 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>Hello World</div>
-    </>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" Component={HomePage} />
+    </Routes>
+
+
+
+  </BrowserRouter>;
 }
 
 export default App
