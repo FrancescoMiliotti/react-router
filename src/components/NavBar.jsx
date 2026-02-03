15a8bf33712d86fba7fda32496f7e58d85a6a38c
francesco.miliotti <francesco.miliotti@email.com>
2026-02-03 19:32:36 +0100

    modificato css navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,15 @@ import { Link, NavLink } from "react-router-dom"
 
 function NavBar() {
     return <nav className="navbar">
-        <NavLink className= "btn" to="/">HomePage</NavLink>
-        <NavLink className="btn" to="/Prodotti">Elenco Prodotti</NavLink>
-        <NavLink className="btn" to="/ChiSiamo">Chi Siamo</NavLink>
-        <NavLink className="btn" to="/DettaglioProdotti">Dettaglio Prodotti</NavLink>
-        
-</nav>
+        <h1>My Site</h1>
+        <nav>
+            <NavLink className="btn" to="/">HomePage</NavLink>
+            <NavLink className="btn" to="/Prodotti">Elenco Prodotti</NavLink>
+            <NavLink className="btn" to="/ChiSiamo">Chi Siamo</NavLink>
+            <NavLink className="btn" to="/DettaglioProdotti">Dettaglio Prodotti</NavLink>
+        </nav>
+
+    </nav>
 }
 
-    export default NavBar
+export default NavBar
